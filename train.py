6b8845d3173c069e380a1ae1b028a8318c59b97e
life-tushar1 <life.tushar@gmail.com>
2017-12-26 03:37:52 +0530

    test5
    --
    Training Step: 93  | total loss: 1.68893 | time: 19.455s
    | Adam | epoch: 001 | loss: 1.68893 - acc: 0.2915 | val_loss: 1.69587 -
    val_acc: 0.2949 -- iter: 5910/5910
    --
    Training Step: 186  | total loss: 1.59842 | time: 20.320s
    | Adam | epoch: 002 | loss: 1.59842 - acc: 0.3094 | val_loss: 1.58587 -
    val_acc: 0.3252 -- iter: 5910/5910
    --
    Training Step: 279  | total loss: 1.54318 | time: 19.254s
    | Adam | epoch: 003 | loss: 1.54318 - acc: 0.3210 | val_loss: 1.51909 -
    val_acc: 0.3325 -- iter: 5910/5910
    --
    Training Step: 372  | total loss: 1.54118 | time: 19.249s
    | Adam | epoch: 004 | loss: 1.54118 - acc: 0.3210 | val_loss: 1.45748 -
    val_acc: 0.3264 -- iter: 5910/5910
    --
    Training Step: 465  | total loss: 1.41997 | time: 19.759s
    | Adam | epoch: 005 | loss: 1.41997 - acc: 0.3674 | val_loss: 1.36364 -
    val_acc: 0.4125 -- iter: 5910/5910
    --
    Training Step: 500  | total loss: 1.35344 | time: 9.803s
    | Adam | epoch: 006 | loss: 1.35344 - acc: 0.4000 | val_loss: 1.33591 -
    val_acc: 0.4149 -- iter: 2240/5910
    --
    Training Step: 558  | total loss: 1.32764 | time: 23.495s
    | Adam | epoch: 006 | loss: 1.32764 - acc: 0.4556 | val_loss: 1.26549 -
    val_acc: 0.4706 -- iter: 5910/5910
    --
    Training Step: 651  | total loss: 1.19058 | time: 19.485s
    | Adam | epoch: 007 | loss: 1.19058 - acc: 0.4930 | val_loss: 1.07341 -
    val_acc: 0.5433 -- iter: 5910/5910
    --
    Training Step: 744  | total loss: 1.12634 | time: 19.351s
    | Adam | epoch: 008 | loss: 1.12634 - acc: 0.5335 | val_loss: 1.01503 -
    val_acc: 0.5756 -- iter: 5910/5910
    --
    Training Step: 837  | total loss: 0.93407 | time: 19.144s
    | Adam | epoch: 009 | loss: 0.93407 - acc: 0.6293 | val_loss: 0.96001 -
    val_acc: 0.5912 -- iter: 5910/5910
    --
    Training Step: 930  | total loss: 0.92196 | time: 19.218s
    | Adam | epoch: 010 | loss: 0.92196 - acc: 0.6236 | val_loss: 0.85404 -
    val_acc: 0.6523 -- iter: 5910/5910
    --

--- train.py
+++ train.py
@@ -77,13 +77,7 @@ from tflearn.layers.estimator import regression
 
 convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
 
-convnet = conv_2d(convnet, 32, 2, activation='relu')
-convnet = max_pool_2d(convnet, 2)
-
-convnet = conv_2d(convnet, 64, 2, activation='relu')
-convnet = max_pool_2d(convnet, 2)
-
-convnet = conv_2d(convnet, 128, 2, activation='relu')
+convnet = conv_2d(convnet, 50, 2, activation='relu')
 convnet = max_pool_2d(convnet, 2)
 
 convnet = conv_2d(convnet, 32, 2, activation='relu')
@@ -94,6 +88,26 @@ convnet = max_pool_2d(convnet, 2)
 
 convnet = conv_2d(convnet, 128, 2, activation='relu')
 convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 256, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 50, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 32, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 64, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 128, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 256, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+
 
 
 
