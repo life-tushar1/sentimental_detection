d12fb24bbcbc4e519d4ff1ec03dd1d36fa2740cc
life-tushar1 <life.tushar@gmail.com>
2017-12-26 02:33:42 +0530

    training and bug removal
    total loss: 17.44296
    epoch: 010
    val_acc: 0.2415

--- train.py
+++ train.py
@@ -88,8 +88,23 @@ convnet = dropout(convnet, 0.8)
 convnet = fully_connected(convnet, 8, activation='softmax')
 convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
 
-model = tflearn.DNN(convnet)
+model = tflearn.DNN(convnet,tensorboard_dir='log')
 
-if os.path.exist('{}.meta'.format(MODEL_NAME)):
+if os.path.exists('{}.meta'.format(MODEL_NAME)):
     model.load(MODEL_NAME)
     print "model loaded"
+
+
+train=train_data[:-500]
+test=train_data[:-500]
+
+#fit
+X=np.array([i[0]for i in train]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+Y=[i[1]for i in train]
+
+#test accuracy
+test_x=np.array([i[0]for i in train]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+test_y=[i[1]for i in test]
+
+model.fit({'input': X}, {'targets': Y}, n_epoch=10, validation_set=({'input': test_x}, {'targets': test_y}),
+    snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
