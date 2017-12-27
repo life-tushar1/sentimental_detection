5a7b372d12ad5abf8c5832e5c68cc10a2d18e66e
life-tushar1 <life.tushar@gmail.com>
2017-12-28 00:50:28 +0530

    minor bug removal in train.py and recreate a model test 5
    Training samples: 5910
    Validation samples: 500
    --
    Training Step: 93  | total loss: 1.72501 | time: 11.470s
    | Adam | epoch: 001 | loss: 1.72501 - acc: 0.2618 | val_loss: 1.68563 -
    val_acc: 0.3220 -- iter: 5910/5910
    --
    Training Step: 186  | total loss: 1.64163 | time: 11.042s
    | Adam | epoch: 002 | loss: 1.64163 - acc: 0.2929 | val_loss: 1.55244 -
    val_acc: 0.2940 -- iter: 5910/5910
    --
    Training Step: 279  | total loss: 1.56954 | time: 11.031s
    | Adam | epoch: 003 | loss: 1.56954 - acc: 0.3397 | val_loss: 1.55112 -
    val_acc: 0.3180 -- iter: 5910/5910
    --
    Training Step: 372  | total loss: 1.45669 | time: 11.671s
    | Adam | epoch: 004 | loss: 1.45669 - acc: 0.3632 | val_loss: 1.50493 -
    val_acc: 0.3460 -- iter: 5910/5910
    --
    Training Step: 465  | total loss: 1.38451 | time: 12.135s
    | Adam | epoch: 005 | loss: 1.38451 - acc: 0.3950 | val_loss: 1.38638 -
    val_acc: 0.4280 -- iter: 5910/5910
    --
    Training Step: 500  | total loss: 1.37704 | time: 5.282s
    | Adam | epoch: 006 | loss: 1.37704 - acc: 0.4318 | val_loss: 1.32534 -
    val_acc: 0.4460 -- iter: 2240/5910
    --
    Training Step: 558  | total loss: 1.28677 | time: 13.509s
    | Adam | epoch: 006 | loss: 1.28677 - acc: 0.4717 | val_loss: 1.23491 -
    val_acc: 0.4860 -- iter: 5910/5910
    --
    Training Step: 651  | total loss: 1.15540 | time: 12.513s
    | Adam | epoch: 007 | loss: 1.15540 - acc: 0.5173 | val_loss: 1.19005 -
    val_acc: 0.5040 -- iter: 5910/5910
    --
    Training Step: 744  | total loss: 1.09769 | time: 11.144s
    | Adam | epoch: 008 | loss: 1.09769 - acc: 0.5616 | val_loss: 0.00000 -
    val_acc: 0.0000 -- iter: 5910/5910
    --
    Training Step: 837  | total loss: 0.95897 | time: 12.675s
    | Adam | epoch: 009 | loss: 0.95897 - acc: 0.5900 | val_loss: 0.97771 -
    val_acc: 0.5980 -- iter: 5910/5910
    --
    Training Step: 930  | total loss: 0.95292 | time: 11.239s
    | Adam | epoch: 010 | loss: 0.95292 - acc: 0.6073 | val_loss: 1.01153 -
    val_acc: 0.5900 -- iter: 5910/5910

--- face_detect.py
+++ face_detect.py
@@ -1,25 +1,92 @@
 import cv2
 import numpy as np
 import sys
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+import tensorflow
 #test=face_cascade.load('haarcascade_frontalface_default.xml')
 #print(test)
-fc=cv2.CascadeClassifier('/Users/tusharsharma/opencv-3.2.0/data/haarcascades/haarcascade_frontalface_default.xml')
-mouth=cv2.CascadeClassifier('/Users/tusharsharma/opencv-3.2.0/data/haarcascades/haarcascade_Smile.xml')
+font=cv2.FONT_HERSHEY_SIMPLEX
+LR=1e-3
+IMG_SIZE=50
+fc=cv2.CascadeClassifier('/Users/tusharsharma/opencv-3.2.0/data/haarcascades/haarcascade_upperbody.xml')
+MODEL_NAME ='sentimentaldetection-{}-{}'.format(LR,'test-4-conv-basic')
+
+convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
+
+convnet = conv_2d(convnet, 32, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 64, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 32, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 64, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 32, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 64, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 32, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 64, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+
+
+
+
+
+convnet = fully_connected(convnet, 1024, activation='relu')
+convnet = dropout(convnet, 0.8)
+
+convnet = fully_connected(convnet, 8, activation='softmax')
+convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+model = tflearn.DNN(convnet,tensorboard_dir='log')
+
+model.load(MODEL_NAME)
 cam=cv2.VideoCapture(0)
 while True:
     ret, frame = cam.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    face=fc.detectMultiScale(gray,1.3,5)
-    lips=mouth.detectMultiScale(gray,6,20)#minSize=(23,25))
-    #lips=mouth.detectMultiScale(gray)
-    print len(face)
-    #sys.stdout.write("\r{0}".format(len(face)))
-    #sys.stdout.flush()
-    #sys.stdout.write("\r{0}".format(len(face)))
-    sys.stdout.flush()
-    for (a,b,c,d) in face:
-        cv2.rectangle(frame,(a,b),(a+c,b+d),(0,0,255),2)
+    frame2=cv2.resize(frame,(100,100))
+    gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    face=fc.detectMultiScale(gray,1.01,5)
 
+    for (a,b,c,d) in face:#x,y,width,height
+        cv2.rectangle(frame2,(a,b),(a+c,b+d),(0,0,255),2)
+        img2=frame2[b:b+d,a:a+c]
+        img2=cv2.resize(cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY),(IMG_SIZE,IMG_SIZE))
+        cv2.imshow('test',img2)
+        cvt=np.array(img2)
+        data=cvt.reshape(IMG_SIZE,IMG_SIZE,1)
+        model_out=model.predict([data])[0]
+        if np.argmax(model_out)==1:
+            str_label='anger'
+        elif np.argmax(model_out)==2:
+            str_label='contempt'
+        elif np.argmax(model_out)==3:
+            str_label='disgust'
+        elif np.argmax(model_out)==4:
+            str_label='fear'
+        elif np.argmax(model_out)==5:
+            str_label='happiness'
+        elif np.argmax(model_out)==6:
+            str_label='sadness'
+        elif np.argmax(model_out)==7:
+            str_label='surprise'
+        elif np.argmax(model_out)==0:
+            str_label='neutral'
+        print str_label
+        cv2.putText(frame,str_label,(0,50),font,0.8,(0,255,0),2,cv2.LINE_AA)
     '''for (a,b,c,d) in lips:
         #if b<400:
             cv2.rectangle(frame,(a,b),(a+c,b+d),(0,255,0),2)'''
--- train.py
+++ train.py
@@ -14,7 +14,7 @@ TEST_DIR='/users/tusharsharma/desktop/project/sentimental_data/test'
 IMG_SIZE=50
 LR=1e-3
 
-MODEL_NAME ='sentimentaldetection-{}-{}'.format(LR,'test-4-conv-basic')
+MODEL_NAME ='sentimentaldetection-{}-{}'.format(LR,'test-5-conv-basic')
 
 #[neutral,anger,contempt,disgust,fear,happiness,sadness,surprise]
 def label_img(img):
@@ -110,10 +110,11 @@ convnet = max_pool_2d(convnet, 2)
 
 
 convnet = fully_connected(convnet, 1024, activation='relu')
-convnet = dropout(convnet, 0.8)
+convnet = dropout(convnet, 0.5)
 
 convnet = fully_connected(convnet, 8, activation='softmax')
 convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+convnet = dropout(convnet, 0.5)
 
 model = tflearn.DNN(convnet,tensorboard_dir='log')
 
@@ -122,16 +123,53 @@ if os.path.exists('{}.meta'.format(MODEL_NAME)):
     print "model loaded"
 
 
-'''train=train_data[:-500]
-test=train_data[-500:]'''
+train=train_data[:-500]
+test=train_data[-500:]
 
 #fit
 X=np.array([i[0]for i in train]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
 Y=[i[1] for i in train]
 
 #test accuracy
-test_x=np.array([i[0]for i in train]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+test_x=np.array([i[0]for i in test]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
 test_y=[i[1]for i in test]
 
 model.fit({'input': X}, {'targets': Y}, n_epoch=10, validation_set=({'input': test_x}, {'targets': test_y}),
     snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+
+model.save(MODEL_NAME)
+'''
+import matplotlib.pyplot as plt
+#test_data=process_test_data()
+test_data=np.load('test_data.npy')
+fig=plt.figure()
+#[neutral,anger,contempt,disgust,fear,happiness,sadness,surprise]
+for num,data in enumerate(test_data[:12]):
+    img_num=data[1]
+    img_data=data[0]
+    y=fig.add_subplot(3,4,num+1)
+    orig=img_data
+    data=img_data.reshape(IMG_SIZE,IMG_SIZE,1)
+    model_out=model.predict([data])[0]
+    if np.argmax(model_out)==1:
+        str_label='anger'
+    elif np.argmax(model_out)==2:
+        str_label='contempt'
+    elif np.argmax(model_out)==3:
+        str_label='disgust'
+    elif np.argmax(model_out)==4:
+        str_label='fear'
+    elif np.argmax(model_out)==5:
+        str_label='happiness'
+    elif np.argmax(model_out)==6:
+        str_label='sadness'
+    elif np.argmax(model_out)==7:
+        str_label='surprise'
+    elif np.argmax(model_out)==0:
+        str_label='neutral'
+    y.imshow(orig,cmap='gray')
+    plt.title(str_label)
+    y.axes.get_xaxis().set_visible(False)
+    y.axes.get_yaxis().set_visible(False)
+plt.show()
+'''
